05b14307cdaa8fedcbca0b86ebf7555b6fa9cce1
Brandy Mello <brandyamello@gmail.com>
2019-10-03 12:18:38 -0600

    Add a seed file for countries

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
 // Update with your config settings.
-
 module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/byob',
-    useNullAsDefault: true,
-    migrations: {
-      directory: './db/migrations'
-    }
-  }
-
+    development: {
+      client: 'pg',
+      connection: 'postgres://localhost/byob',
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds: {
+        directory: '.db/seeds/dev'
+      },
+      useNullAsDefault: true
+    } 
 };
