3e2ee71a981b027496e1d1e83ec06ad58a6184f4
Brandy Mello <brandyamello@gmail.com>
2019-10-03 12:06:47 -0600

    Fix the territory table by dropping population and adding it as an integer column

---
+++ db/migrations/20191003115715_drop-population-column-replace.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('dependencies_or_territories', (table) => {
+    table.dropColumn('population');
+    table.integer('territory_population');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('dependencies_or_territories', (table) => {
+    table.string('population');
+  })
+};
