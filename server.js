f15d7dc38be951811673a9c50a59bb7f86f7213b
Brandy Mello <brandyamello@gmail.com>
2019-10-06 16:06:31 -0600

    Change id assignment

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/api/v1/territories', (request, response) => {
 
   database('dependencies_or_territories').insert(countryTerritory, 'id')
     .then(territory => {
-      response.status(201).json({ id: territory[0] })
+      response.status(201).json({ id: countryTerritory[0] })
     .catch(error => {
       response.status(500).json({ error });
     })
